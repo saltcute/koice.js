f9f71a82dd9910be6111a68e7299cf6b7b940f62
saltcute <me@mizuno.ai>
2024-07-27 23:40:55 +0000

    fix: reconnect automatically if exited unexpectedly

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import ffmpeg, { FfmpegCommand } from "fluent-ffmpeg";
 
 import { Readable as ReadableStream } from "stream";
-import Kasumi from "kasumi.js";
+import Kasumi, { SystemMessageEvent } from "kasumi.js";
 import EventEmitter2 from "eventemitter2";
 
 export interface RawEmisions {
@@ -21,6 +21,16 @@ export interface IStreamOptions {
     password?: string;
     inputCodec?: string;
     forceRealSpeed?: boolean;
+    bitrateFactor?: number;
+}
+
+export interface IUserLeaveVoiceChannelEventExtra {
+    type: "exited_channel";
+    body: {
+        user_id: string;
+        channel_id: string;
+        exited_at: number;
+    };
 }
 
 export class Koice extends EventEmitter2 {
@@ -30,7 +40,9 @@ export class Koice extends EventEmitter2 {
     private stream: ReadableStream = new ReadableStream({
         read() {},
     });
+    private fileHead?: any;
     push(chunk: any) {
+        if (!this.fileHead) this.fileHead = chunk;
         this.stream.push(chunk);
     }
 
@@ -62,7 +74,12 @@ export class Koice extends EventEmitter2 {
         if (binary) this.ffPath = binary;
         ffmpeg.setFfmpegPath(this.ffPath);
 
+        this.client.on("event.system", this.disconnectionHandler);
         this.on("close", this.onclose);
+    }
+    private streamOptions?: IStreamOptions;
+    public setStreamOptions(options?: IStreamOptions) {
+        this.streamOptions = options;
     }
     /**
      * Start streaming audio in the voice chat
@@ -76,30 +93,34 @@ export class Koice extends EventEmitter2 {
         binary?: string
     ): Promise<Koice | null> {
         const self = new Koice(client, targetChannelId, binary);
-        if (await self.startStream(options)) return self;
+        self.setStreamOptions(options);
+        if (await self.startStream()) return self;
         else return null;
     }
-    private async startStream(options?: IStreamOptions): Promise<boolean> {
+    private async startStream(): Promise<boolean> {
         this.isClose = false;
 
         const { data, err } = await this.client.API.voice.join(
             this.targetChannelId,
             {
-                password: options?.password,
-                rtcpMux: options?.rtcpMux,
+                password: this.streamOptions?.password,
+                rtcpMux: this.streamOptions?.rtcpMux,
             }
         );
         if (err) return false;
 
         this.ffmpeg = ffmpeg();
         this.ffmpeg.input(this.stream);
-        // if (options?.inputCodec)
-        //     this.ffmpeg.addInputOptions(`-acodec ${options.inputCodec}`);
-        if (options?.forceRealSpeed) this.ffmpeg.withNativeFramerate();
+        if (this.streamOptions?.inputCodec)
+            this.ffmpeg.inputFormat(this.streamOptions.inputCodec);
+        if (this.streamOptions?.forceRealSpeed)
+            this.ffmpeg.withNativeFramerate();
         this.ffmpeg
             .outputOption(["-map 0:a:0"])
             .audioCodec("libopus")
-            .audioBitrate(`${Math.floor(data.bitrate / 1000)}k`)
+            .audioBitrate(
+                `${Math.floor((data.bitrate / 1000) * (this.streamOptions?.bitrateFactor || 1))}k`
+            )
             .audioChannels(2)
             .audioFrequency(48000)
             .outputFormat("tee")
@@ -114,14 +135,26 @@ export class Koice extends EventEmitter2 {
             .on("end", (e) => {
                 this.close(e);
             });
+        if (this.fileHead) this.push(this.fileHead);
         return true;
     }
+    private disconnectionHandler = async (event: SystemMessageEvent) => {
+        if (event.rawEvent.extra.type == "exited_channel") {
+            const extra: IUserLeaveVoiceChannelEventExtra =
+                event.rawEvent.extra;
+            if (extra.body.user_id == this.client.me.userId) {
+                return this.startStream();
+            }
+        }
+        return false;
+    };
     async close(reason?: any): Promise<void> {
         if (this.ffmpeg) {
             this.ffmpeg.kill("SIGKILL");
             delete this.ffmpeg;
         }
         if (!this.isClose) {
+            this.client.off("event.system", this.disconnectionHandler);
             await this.client.API.voice.leave(this.targetChannelId);
             this.isClose = true;
             this.emit("close", reason);
